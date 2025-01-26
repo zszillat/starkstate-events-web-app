a0358f2aeeba4ad3a9e04c48fe18f8b05134a565
zszillat <zachary.szillat@outlook.com>
2025-01-26 18:27:43 -0500

    homepage routing

--- main/views.py
+++ main/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse('<h1>Welcome to the Homepage!</h1>') 
+    return render(request, 'home.html')
