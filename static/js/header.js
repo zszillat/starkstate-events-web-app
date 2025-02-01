be55d9769d14109263d858030d3067e42896f273
Zachary Szillat <zachary.szillat@outlook.com>
2025-02-01 13:42:30 -0500

    fix menu button

--- static/js/header.js
+++ static/js/header.js
@@ -5,7 +5,8 @@ const mobileMenu = document.getElementById('mobileMenu');
 // Toggle Menu
 menuToggleButton.addEventListener('click', () => {
      state = mobileMenu.style.height
-     if (state === '0px' || state === '0') {
+     console.log(state)
+     if (state === '0px' || state === '0' || state === '') {
         mobileMenu.style.height = mobileMenu.scrollHeight + 'px';
      } else {
         mobileMenu.style.height = '0px'
