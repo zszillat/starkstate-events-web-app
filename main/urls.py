b567ae2c373a7c868057cfcbeb19e8298c003391
Zachary Szillat <zachary.szillat@outlook.com>
2025-02-05 15:50:25 -0500

    Setup URL Routing

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('event', views.event, name='event details'),
+    path('login', views.login, name='login'),
+    path('account', views.account, name='account'),
+    path('new', views.new, name='new'),
+    path('club', views.new, name='club'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return render(request, 'home.html')
+
+def event(request):
+    return render(request, 'event.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def account(request):
+    return render(request, 'account.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+def club(request):
+    return render(request, 'club.html')
