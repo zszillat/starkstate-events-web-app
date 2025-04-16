09362f66139a10887a688b441a446f1e55d087d3
Zachary Szillat <zachary.szillat@outlook.com>
2025-04-16 11:10:51 -0400

    CSS Overhaul

--- static/js/header.js
+++ static/js/header.js
@@ -1,14 +1,9 @@
-// Select elements
-const menuToggleButton = document.getElementById('menuToggleButton');
-const mobileMenu = document.getElementById('mobileMenu');
-
-// Toggle Menu
-menuToggleButton.addEventListener('click', () => {
-     state = mobileMenu.style.height
-     console.log(state)
-     if (state === '0px' || state === '0' || state === '') {
-        mobileMenu.style.height = mobileMenu.scrollHeight + 'px';
-     } else {
-        mobileMenu.style.height = '0px'
-     }
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleButton = document.getElementById('menuToggleButton');
+    const mobileMenu = document.getElementById('mobileMenu');
+  
+    toggleButton.addEventListener('click', () => {
+      mobileMenu.classList.toggle('show');
+    });
+  });
+  
