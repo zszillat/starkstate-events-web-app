d4a63963f72caa645024eba876fe6bca65465b4a
Zachary Szillat <zachary.szillat@outlook.com>
2025-04-19 19:20:24 -0400

    added import/export and updated dummy data

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,51 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from .models import Student, Club, Event, Attendance, Feedback
 
-class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'date', 'club', 'image')
+# Resource Classes
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
 
-class ClubAdmin(admin.ModelAdmin):
+class ClubResource(resources.ModelResource):
+    class Meta:
+        model = Club
+
+class EventResource(resources.ModelResource):
+    class Meta:
+        model = Event
+
+class AttendanceResource(resources.ModelResource):
+    class Meta:
+        model = Attendance
+
+class FeedbackResource(resources.ModelResource):
+    class Meta:
+        model = Feedback
+
+# Admin Classes
+@admin.register(Student)
+class StudentAdmin(ImportExportModelAdmin):
+    resource_class = StudentResource
+    list_display = ('id', 'first_name', 'last_name', 'email')
+
+@admin.register(Club)
+class ClubAdmin(ImportExportModelAdmin):
+    resource_class = ClubResource
     list_display = ('name', 'leader', 'pfp')
 
-admin.site.register(Student)
-admin.site.register(Club, ClubAdmin)
-admin.site.register(Event, EventAdmin)
-admin.site.register(Attendance)
-admin.site.register(Feedback)
+@admin.register(Event)
+class EventAdmin(ImportExportModelAdmin):
+    resource_class = EventResource
+    list_display = ('name', 'date', 'club', 'image')
+
+@admin.register(Attendance)
+class AttendanceAdmin(ImportExportModelAdmin):
+    resource_class = AttendanceResource
+    list_display = ('student', 'event')
+
+@admin.register(Feedback)
+class FeedbackAdmin(ImportExportModelAdmin):
+    resource_class = FeedbackResource
+    list_display = ('student', 'event', 'feedback')
--- starkstate_events_web_app/settings.py
+++ starkstate_events_web_app/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'import_export'
 ]
 
 MIDDLEWARE = [
